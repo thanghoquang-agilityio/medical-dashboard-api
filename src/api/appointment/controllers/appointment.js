08568af63fbaa1f77f247c02ce0453ebe6af7f6a
Thang Ho Quang <thang.hoquang@asnet.com.vn>
2024-10-08 15:28:17 +0700

    feat: update custom api

--- src/api/appointment/controllers/appointment.js
+++ src/api/appointment/controllers/appointment.js
@@ -1,9 +1,51 @@
-'use strict';
+"use strict";
 
 /**
  * appointment controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::appointment.appointment');
+module.exports = createCoreController(
+  "api::appointment.appointment",
+  ({ strapi }) => ({
+    async updateUnpublished(ctx) {
+      const { userId } = ctx.params;
+
+      try {
+        const entries = await strapi.db
+          .query("api::appointment.appointment")
+          .findMany({
+            where: {
+              or: [
+                { senderId: { id: userId } },
+                { receiverId: { id: userId } },
+              ],
+            },
+          });
+
+        if (entries.length === 0) {
+          return ctx.send(
+            { message: `No appointments found for user ID ${userId}.` },
+            404
+          );
+        }
+
+        const updatePromises = entries.map((entry) =>
+          strapi.db.query("api::appointment.appointment").update({
+            where: { id: entry.id },
+            data: { publishedAt: null },
+          })
+        );
+
+        await Promise.all(updatePromises);
+
+        ctx.send({
+          message: `Updated unpublished for user ID: ${userId}.`,
+        });
+      } catch (error) {
+        ctx.send({ error: "An error occurred while updating entries." }, 500);
+      }
+    },
+  })
+);
--- src/api/appointment/routes/appointment.js
+++ src/api/appointment/routes/appointment.js
@@ -1,9 +1,32 @@
-'use strict';
+"use strict";
 
 /**
  * appointment router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::appointment.appointment');
+const defaultRouter = createCoreRouter("api::appointment.appointment");
+
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+const myExtraRoutes = [
+  {
+    method: "PUT",
+    path: "/appointments/unpublish/:userId",
+    handler: "appointment.updateUnpublished",
+  },
+];
+
+module.exports = customRouter(defaultRouter, myExtraRoutes);
