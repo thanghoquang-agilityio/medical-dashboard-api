66f185c01f03826ddfc83a2ea36285ab1939beb5
Thang Ho Quang <thang.hoquang@asnet.com.vn>
2024-10-09 17:55:25 +0700

    fix: update unpublish

--- src/api/chemist/controllers/chemist.js
+++ src/api/chemist/controllers/chemist.js
@@ -1,9 +1,43 @@
-'use strict';
+"use strict";
 
 /**
  * chemist controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::chemist.chemist');
+module.exports = createCoreController("api::chemist.chemist", ({ strapi }) => ({
+  async updateUnpublished(ctx) {
+    const { userId } = ctx.params;
+
+    try {
+      const entries = await strapi.db.query("api::chemist.chemist").findMany({
+        where: {
+          users_permissions_user: { id: userId },
+        },
+      });
+
+      if (entries.length === 0) {
+        return ctx.send(
+          { message: `No chemists found for user ID ${userId}.` },
+          404
+        );
+      }
+
+      const updatePromises = entries.map((entry) =>
+        strapi.db.query("api::chemist.chemist").update({
+          where: { id: entry.id },
+          data: { publishedAt: null },
+        })
+      );
+
+      await Promise.all(updatePromises);
+
+      ctx.send({
+        message: `Updated unpublished for user ID: ${userId}.`,
+      });
+    } catch (error) {
+      ctx.send({ error: "An error occurred while updating entries." }, 500);
+    }
+  },
+}));
